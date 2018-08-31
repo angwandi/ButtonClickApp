b721b987272c01fca52c07e41e37807f1eb8ed44
Innocent <31923567+angwandi@users.noreply.github.com>
2018-08-31 19:27:34 +0100

    Add handling screen state change

--- app/src/main/java/app/a2ms/buttonclickapp/MainActivity.kt
+++ app/src/main/java/app/a2ms/buttonclickapp/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-private val TAG = "MainActivity"
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENTS = "TextContent"
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,15 +27,13 @@ class MainActivity : AppCompatActivity() {
         textView?.movementMethod = ScrollingMovementMethod() //works only because we added a scrollbars vertical in xml
         userInput.text.clear()
 
-        button.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Log.d(TAG, "onClick : called")
-                textView?.append(userInput.text)
-                textView?.append("\n")
-                userInput.text.clear() //clear editText after button clicked option 1
-                //userInput.setText("") //clear editText after button clicked option 2
-            }
-        })
+        button.setOnClickListener {
+            Log.d(TAG, "onClick : called")
+            textView?.append(userInput.text)
+            textView?.append("\n")
+            userInput.text.clear() //clear editText after button clicked option 1
+            //userInput.setText("") //clear editText after button clicked option 2
+        }
     }
 
     override fun onStart() {
@@ -46,37 +44,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         Log.d(TAG, "onRestoreInstanceState: called")
-
         super.onRestoreInstanceState(savedInstanceState)
+//        val savedString = savedInstanceState?.getString(TEXT_CONTENTS, "")
+//        textView?.text = savedString
+        textView?.text = savedInstanceState?.getString(TEXT_CONTENTS, "")
     }
 
     override fun onResume() {
         Log.d(TAG, "onResume: called")
-
         super.onResume()
     }
 
     override fun onPause() {
         Log.d(TAG, "onPause: called")
-
         super.onPause()
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         Log.d(TAG, "onSaveInstanceState: called")
-
         super.onSaveInstanceState(outState)
+        outState?.putString(TEXT_CONTENTS, textView?.text.toString())
     }
 
     override fun onStop() {
         Log.d(TAG, "onStop: called")
-
         super.onStop()
+        //save data remotely here
     }
 
     override fun onDestroy() {
         Log.d(TAG, "onDestroy: called")
-
         super.onDestroy()
     }
 
